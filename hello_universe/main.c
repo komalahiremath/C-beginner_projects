e5a0d246c2cb365d937f98e7816a38b23fadd38f
komalahiremath <komalashiremath6@gmail.com>
2024-12-28 18:38:27 +0000

    Add trivial hello universe program

---
+++ hello_universe/main.c
@@ -0,0 +1,13 @@
+/***************************************************
+ * Program: Trivial program to print hello-universe
+ * Author: Komala 
+ **************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+    /* Print Hello Universe on console */
+	printf("hello universe!!\n");
+	return 0;
+}
