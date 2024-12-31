3eeaf4c6f8ab93f3e58fbc8f481812350c3558cc
komalahiremath <komalashiremath6@gmail.com>
2024-12-31 08:42:49 +0000

    fix the indentation

--- nested_for_loop/main.c
+++ nested_for_loop/main.c
@@ -26,21 +26,21 @@ int main()
 	/* NxN box to be constructed */
 	for(k=1;k<=2;k++)
 	{
-			for(l=1;l<=user_input;l++)
-			{
-				printf("*");
-			}
+		for(l=1;l<=user_input;l++)
+		{
+			printf("*");
+		}
+		printf("\n");
+		for(k=1;k<=user_input-2;k++)
+		{
+			printf("*\t*");
 			printf("\n");
-			for(k=1;k<=user_input-2;k++)
-			{
-				printf("*\t*");
-				printf("\n");
-			}
-			for(j=1;j<=user_input;j++)
-			{
-				printf("*");
-			}
-			printf("\n");
+		}
+		for(j=1;j<=user_input;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
 	}
 
 	return 0;
