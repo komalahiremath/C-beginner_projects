16898a0cb4a242061d66a947a9f4d53ad94641c0
komalahiremath <komalashiremath6@gmail.com>
2024-12-31 08:35:51 +0000

    refactor the NxN box application

--- nested_for_loop/main.c
+++ nested_for_loop/main.c
@@ -1,46 +1,47 @@
 /***************************************************
  * Program: Program to print N X N empty box
- * Example output:
-   ++++++++++
-   +   +
-   +   +
-   +   +
-   +   +
-   +   +
-   +   +
-   +   +
-   +   +
-   ++++++++++
  * Author: Komala 
+ * Example output:
+	 ++++++++++
+	 +        +
+	 +        +
+	 +        +
+	 +        +
+	 +        +
+	 +        +
+	 +        +
+	 +        +
+	 ++++++++++
  **************************************************/
-
 
 #include<stdio.h> 
 
 int main()
 {
-   int N, i,j,k,l;
-   printf("Enter a Number");        //to get a positive integer number (N) from the user
-   scanf("%d",&N);                  //address for the variable to store
-   
-   for(k=1;k<=2;k++)                 //nested for loop
-   {
-       for(l=1;l<=N;l++)
-       {
-           printf("*");
-       }
-       printf("\n");
-       for(k=1;k<=N-2;k++)
-       {
-          printf("*\t*");
-          printf("\n");
-       }
-       for(j=1;j<=N;j++)
-        {
-            printf("*");
-        }
-        printf("\n");
-   }
-   
-  return 0; 
+	 int user_input, i,j,k,l;
+	 /* fetch input from the user */
+	 printf("Enter a Number");
+	 scanf("%d",&user_input);
+
+	/* NxN box to be constructed */
+	for(k=1;k<=2;k++)
+	{
+			for(l=1;l<=user_input;l++)
+			{
+				printf("*");
+			}
+			printf("\n");
+			for(k=1;k<=user_input-2;k++)
+			{
+				printf("*\t*");
+				printf("\n");
+			}
+			for(j=1;j<=user_input;j++)
+			{
+				printf("*");
+			}
+			printf("\n");
+	}
+
+	return 0;
 }
